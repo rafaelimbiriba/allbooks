e20aea48cdad2d2e9b5c1064eee147d0facb9f16
Rafael <rafaelimbiriba@gmail.com>
2024-03-05 13:33:21 -0300

    Altera o server.js

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -18,10 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boa-vindas a API do AllBooks")
-  console.log("API disponível através da URL http://localhost:8000")
-=======
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 8e73cc8ef042435fae4f45b1c36436109695b615
+  console.log("Boas vindas a API do AllBooks")
+  console.log("API disponivel através da URL http://localhost:8000")
 })
